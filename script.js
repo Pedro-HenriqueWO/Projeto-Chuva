3662bd91ff79e776e602fc6680567210b168f089
Pedro-HenriqueWO <pedro.albuquerque2020@outlook.com>
2022-05-27 10:24:49 -0300

    ATT

--- script.js
+++ script.js
@@ -12,16 +12,3 @@ showButton.onclick = function () {
     }
 };
 
-
-let showButton = document.querySelector('#showButton');
-
-showButton.onclick = function () {
-    let div = document.getElementById('topics');
-    behavior: 'smooth'
-    if (div.style.maxHeight <= '11%') {
-        div.style.maxHeight = '26%';
-    }
-    else {
-        div.style.maxHeight = '11%';
-    }
-};
